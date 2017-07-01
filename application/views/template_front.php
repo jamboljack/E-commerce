d0cacfc4392727fa331111918e934558fcb5108c
Jama' Muttaqin <jama.muttaqin@gmail.com>
2017-07-01 16:38:24 +0700

    Tawk Chat Online

--- application/views/template_front.php
+++ application/views/template_front.php
@@ -33,7 +33,7 @@ var Tawk_API=Tawk_API||{}, Tawk_LoadStart=new Date();
 (function(){
 var s1=document.createElement("script"),s0=document.getElementsByTagName("script")[0];
 s1.async=true;
-s1.src='https://embed.tawk.to/594e5973e9c6d324a473721e/default';
+s1.src='https://embed.tawk.to/59572e3950fd5105d0c836a9/default';
 s1.charset='UTF-8';
 s1.setAttribute('crossorigin','*');
 s0.parentNode.insertBefore(s1,s0);
